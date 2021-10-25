24cd697874ff442628d6127e4c9e29df029d835d
Shay Mesh <shaymesh420@gmail.com>
2021-10-25 21:45:25 +0300

    Project completed.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
-import React from 'react';
-import { ApolloClient, ApolloProvider, InMemoryCache, HttpLink } from '@apollo/client';
-import './App.css';
+import React from "react";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  HttpLink,
+} from "@apollo/client";
+import "./App.css";
 
 //components
-import Routes from './components/Routes';
+import Routes from "./components/Routes";
 
-const { REACT_APP_URI } = process.env;
+const REACT_APP_URI = "http://localhost:4000/graphql";
 
 const App = () => {
   const cache = new InMemoryCache();
   const link = new HttpLink({
     uri: REACT_APP_URI,
     headers: {
-      "Cross-Origin-Resource-Policy": 'cross-origin',
-      "Cross-Origin-Embedder-Policy": 'require-corp',
-      "Cross-Origin-Opener-Policy": 'same-origin'
-    }
-  })
+      "Cross-Origin-Resource-Policy": "cross-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin",
+    },
+  });
   const client = new ApolloClient({
     cache,
-    link
+    link,
   });
 
   return (
     <React.Fragment>
       <div className="shape"></div>
-      <div className='app'>
+      <div className="app">
         <ApolloProvider client={client}>
           <Routes />
         </ApolloProvider>
       </div>
     </React.Fragment>
-  )
-}
-
+  );
+};
 
 export default App;
--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,61 +1,58 @@
-import React, { useState } from 'react'
-import { gql, useQuery, useMutation } from '@apollo/client';
+import React, { useState } from "react";
+import { gql, useQuery, useMutation } from "@apollo/client";
 
 //components
-import Error from '../Error';
-import Loading from '../Loading';
-import CardList from '../CardList';
-
+import Error from "../Error";
+import Loading from "../Loading";
+import CardList from "../CardList";
 
 const CARDS_QUERY = gql`
-    query {
-        cards{
-          _id
-          name
-          sold
+  query {
+    cards {
+      _id
+      name
+      sold
     }
   }
 `;
 
 const CHANGE_STATUS_MUTATION = gql`
-mutation ChangeStatus($cardId: ID!){
+  mutation ChangeStatus($cardId: ID!) {
     changeStatus(id: $cardId) {
-        sold
+      sold
     }
-}
+  }
 `;
 
 const Cards = ({ match }) => {
-    const { path } = match;
-    const bool = path === '/cards/sold' ? true : false;
+  const { path } = match;
+  const bool = path === "/cards/sold" ? true : false;
 
-    const [data, setData] = useState({ cards: [] });
-    const { loading, error } = useQuery(CARDS_QUERY, { onCompleted: setData });
-    const [changeStatusMutation] = useMutation(CHANGE_STATUS_MUTATION);
+  const [data, setData] = useState({ cards: [] });
+  const { loading, error } = useQuery(CARDS_QUERY, { onCompleted: setData });
+  const [changeStatusMutation] = useMutation(CHANGE_STATUS_MUTATION);
 
-    if (error) return <Error error={error} component={'Cards'} />
+  if (error) return <Error error={error} component={"Cards"} />;
 
-    if (loading) return <Loading />
+  if (loading) return <Loading />;
 
+  const changeStatus = (card) => {
+    changeStatusMutation({ variables: { cardId: card._id } });
+    const cardIndex = data.cards.findIndex((c) => c._id === card._id);
+    const newCard = { ...card, sold: !card.sold };
+    const newArr = [...data.cards];
+    newArr.splice(cardIndex, 1, newCard);
+    setData({ cards: newArr });
+  };
 
-    const changeStatus = (card) => {
-        changeStatusMutation({ variables: { cardId: card._id } });
-        const cardIndex = data.cards.findIndex(c => c._id === card._id);
-        const newCard = { ...card, sold: !card.sold }
-        const newArr = [...data.cards];
-        newArr.splice(cardIndex, 1, newCard);
-        setData({ cards: newArr });
-    }
+  const arr = bool
+    ? data.cards.filter((card) => card.sold)
+    : data.cards.filter((card) => !card.sold);
 
+  console.log(data);
 
-    const arr = bool
-        ? data.cards.filter(card => card.sold)
-        : data.cards.filter(card => !card.sold)
-
-    return (
-        <CardList cards={arr} changeStatus={changeStatus} />
-    )
-}
-
+  return <CardList cards={arr} changeStatus={changeStatus} />;
+};
 
 export default Cards;
+
